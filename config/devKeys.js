38b16153227fa94bee2c0849b2f1883844d6bcda
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-06-24 18:37:17 +1000

    live key

--- config/devKeys.js
+++ config/devKeys.js
@@ -1,11 +1,11 @@
 //Development Keys
 module.exports = {
 	googleClientID:
-		"284598256078-q8itp2111h9vdd76bbcf0juttmjpnge7.apps.googleusercontent.com",
-	googleClientSecret: "s47YjlHH5RroZ3b5-a5tr96r",
+		"736761210251-uo8cgfu5b5mgqk8400h809h77cmcfg2l.apps.googleusercontent.com",
+	googleClientSecret: "ISsy8biZVe5COFWXC5qZSyEI",
 	mongoURI:
 		"mongodb+srv://billy:billy@cluster0-xvouw.mongodb.net/test?retryWrites=true&w=majority",
 	cookieKey: "vekjnvkjervjernvjkernvjenr",
-	stripePublishableKey: "pk_test_yVP1z9yIyViNoRUxvYviIi4k00Xi2K1Twi",
-	stripeSecretKey: "sk_test_2jzFcv75eNutFa79A2tQNTc1009FtIJZiQ"
+	stripePublishableKey: "pk_live_qARXxuAAjn8jU9c311KBpRi0",
+	stripeSecretKey: "sk_live_akJGfiZc7KT87K60ptWUdcgc00qKAko2m0"
 };
